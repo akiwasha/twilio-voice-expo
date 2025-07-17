4eb34c6723cc26c4242a3410d70fdbdbc4062285
Pierre-Laurent Vincent <plvincent.pro@gmail.com>
2025-07-17 18:15:41 +0200

    chore(expo): add expo support

---
+++ expo-config-plugin/ios.js
@@ -0,0 +1,21 @@
+const { withInfoPlist, withEntitlements } = require('@expo/config-plugins');
+
+function withTwilioVoiceIOS(config) {
+  // Ajouter les permissions nécessaires
+  config = withInfoPlist(config, (config) => {
+    config.modResults.NSMicrophoneUsageDescription =
+      'Cette application a besoin du microphone pour les appels vocaux';
+    config.modResults.UIBackgroundModes = ['audio', 'voip'];
+    return config;
+  });
+
+  // Ajouter les entitlements VoIP
+  config = withEntitlements(config, (config) => {
+    config.modResults['com.apple.developer.pushkit'] = ['voip'];
+    return config;
+  });
+
+  return config;
+}
+
+module.exports = withTwilioVoiceIOS;
